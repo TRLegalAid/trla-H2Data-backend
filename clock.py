bc0b241f4c56fd355ec74bc5c39dcfaeb197f524
alpiazza13 <alexpiazza2000@gmail.com.com>
2020-07-19 15:55:49 -0400

    hello

--- clock.py
+++ clock.py
@@ -3,19 +3,28 @@ import pandas as pd
 from sqlalchemy import create_engine
 engine = create_engine('postgres://txmzafvlwebrcr:df20d17265cf81634b9f689187248524a6fd0d56222985e2f422c71887ec6ec0@ec2-34-224-229-81.compute-1.amazonaws.com:5432/dbs39jork6o07d')
 
-sched = BlockingScheduler()
-
-@sched.scheduled_job('interval', minutes=1)
-def timed_job():
+def schdeuled_job():
     df = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
-    df.to_sql("new", engine, if_exists='append', index=False)
-    print("helloooooo")
+    df.to_sql("new2", engine, if_exists='append', index=False)
+    print("maybe this succeeded")
+
+sched = BlockingScheduler()
+sched.add_job(scheduled_job, 'cron', hour='15', minute='59')
+
+# @sched.scheduled_job('interval', minutes=1)
+# def timed_job():
+#     df = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
+#     df.to_sql("new", engine, if_exists='append', index=False)
+#     print("helloooooo")
+
+# this works locally but not on heroku
 # when wake up there should be 4 of them (6 rows)
 # @sched.scheduled_job('cron', hour='15', minute='45')
 # def schdeuled_job():
 #     df = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
 #     df.to_sql("new2", engine, if_exists='append', index=False)
 #     print("maybe this succeeded")
+
 
 # @sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
 # def scheduled_job():
