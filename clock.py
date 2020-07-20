c1168d0d353112e8ea834223a7b53f8d8b8b9502
alpiazza13 <alexpiazza2000@gmail.com.com>
2020-07-19 21:24:25 -0400

    adsasd'

--- clock.py
+++ clock.py
@@ -1,6 +1,7 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 import pandas as pd
 from sqlalchemy import create_engine
+import client
 import geocodio
 client = geocodio.GeocodioClient("454565525ee5444fefef2572155e155e5248221")
 import requests
