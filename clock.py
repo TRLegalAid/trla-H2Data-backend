e5d8171b13b658e4c277fa70a265f7267e1864fe
alpiazza13 <alexpiazza2000@gmail.com.com>
2020-07-19 21:21:28 -0400

    not gooood

--- clock.py
+++ clock.py
@@ -1,10 +1,9 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 import pandas as pd
 from sqlalchemy import create_engine
-from geocodio import GeocodioClient
-cl = GeocodioClient("454565525ee5444fefef2572155e155e5248221")
+import geocodio
+client = geocodio.GeocodioClient("454565525ee5444fefef2572155e155e5248221")
 import requests
-from client import Client
 
 engine = create_engine('postgres://txmzafvlwebrcr:df20d17265cf81634b9f689187248524a6fd0d56222985e2f422c71887ec6ec0@ec2-34-224-229-81.compute-1.amazonaws.com:5432/dbs39jork6o07d')
 
@@ -12,7 +11,7 @@ def scheduled_job():
     df = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
     df.to_sql("new3", engine, if_exists='append', index=False)
     print("maybe this succeeded")
-    geocoded = cl.geocode("11 Cortlandt Manor Rd, Katonah NY 10536")
+    geocoded = client.geocode("11 Cortlandt Manor Rd, Katonah NY 10536")
     print(geocoded.coords)
     jobs = requests.get("https://api.apify.com/v2/acts/eytaog~apify-dol-actor-latest/runs/last/dataset/items?token=ftLRsXTA25gFTaCvcpnebavKw").json()
     print(len(jobs))
