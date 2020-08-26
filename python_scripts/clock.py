a2c16350adfa7b2d29bc3d80d971cce80fabc255
alpiazza13 <alexpiazza2000@gmail.com.com>
2020-08-26 03:01:18 -0400

    it worked last time by some miracle, now testing again

--- python_scripts/clock.py
+++ python_scripts/clock.py
@@ -3,6 +3,7 @@ import os
 import pandas as pd
 from sqlalchemy import create_engine
 import geocodio
+print(dir(geocodio))
 client = geocodio.GeocodioClient("454565525ee5444fefef2572155e155e5248221")
 
 def get_secret_variables():
@@ -12,15 +13,16 @@ def get_secret_variables():
     return os.getenv("DATABASE_URL"), os.getenv("GEOCODIO_API_KEY")
 
 # client = geocodio.GeocodioClient(get_secret_variables()[1])
-geocoded_location = client.geocode("42370 Bob Hope Drive, Rancho Mirage CA")
-print(geocoded_location.coords)
+# geocoded_location = client.geocode("42370 Bob Hope Drive, Rancho Mirage CA")
+# print(geocoded_location.coords)
 
-# def scheduled_job():
-#     geocoded_location = client.geocode("42370 Bob Hope Drive, Rancho Mirage CA")
-#     print(geocoded_location.coords)
-#
-# sched = BlockingScheduler()
-# # change minutes=2 to days=1
-# sched.add_job(scheduled_job, 'interval', seconds=3, start_date='2020-07-19 22:10:00', timezone='US/Eastern')
-#
-# sched.start()
+def scheduled_job():
+    print('and again...')
+    geocoded_location = client.geocode("42370 Bob Hope Drive, Rancho Mirage CA")
+    print(geocoded_location.coords)
+
+sched = BlockingScheduler()
+# change minutes=2 to days=1
+sched.add_job(scheduled_job, 'interval', seconds=5, start_date='2020-08-25 22:10:00', timezone='US/Eastern')
+
+sched.start()
