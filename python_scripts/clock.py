b2328c54ab10f324bc21db90f0eff1cd0980c9e9
alpiazza13 <alexpiazza2000@gmail.com.com>
2020-08-26 02:55:00 -0400

    trying to revert to last working version

--- python_scripts/clock.py
+++ python_scripts/clock.py
@@ -1,6 +1,9 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 import os
+import pandas as pd
+from sqlalchemy import create_engine
 import geocodio
+client = geocodio.GeocodioClient("454565525ee5444fefef2572155e155e5248221")
 
 def get_secret_variables():
     if os.getenv("LOCAL_DEV") == "true":
@@ -8,7 +11,7 @@ def get_secret_variables():
         return secret_variables.DATABASE_URL, secret_variables.GEOCODIO_API_KEY
     return os.getenv("DATABASE_URL"), os.getenv("GEOCODIO_API_KEY")
 
-client = geocodio.GeocodioClient(get_secret_variables()[1])
+# client = geocodio.GeocodioClient(get_secret_variables()[1])
 geocoded_location = client.geocode("42370 Bob Hope Drive, Rancho Mirage CA")
 print(geocoded_location.coords)
 
