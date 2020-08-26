90b419fc3afb31d761d4d515a3b2cc8bc63c0175
alpiazza13 <alexpiazza2000@gmail.com.com>
2020-08-26 02:01:28 -0400

    removes runtime.txt

--- python_scripts/clock.py
+++ python_scripts/clock.py
@@ -1,18 +1,16 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
-import pandas as pd
-from sqlalchemy import create_engine
-import geocodio
-import requests
-import helpers
 import os
-
+from helpers import get_secret_variables
+print(get_secret_variables()[1])
+from geocodio import GeocodioClient
+client = GeocodioClient(get_secret_variables()[1])
 
 def scheduled_job():
-    print(helpers.get_secret_variables())
-
+    geocoded_location = client.geocode("42370 Bob Hope Drive, Rancho Mirage CA")
+    print(geocoded_location.coords)
 
 sched = BlockingScheduler()
 # change minutes=2 to days=1
-sched.add_job(scheduled_job, 'interval', seconds=8, start_date='2020-07-19 22:10:00', timezone='US/Eastern')
+sched.add_job(scheduled_job, 'interval', seconds=5, start_date='2020-07-19 22:10:00', timezone='US/Eastern')
 
 sched.start()
