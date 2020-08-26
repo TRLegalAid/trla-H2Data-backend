4a479243cb5338c7236c482bc099b56ba75dee5a
alpiazza13 <alexpiazza2000@gmail.com.com>
2020-08-26 03:04:44 -0400

    version that successfully geocodes non-locally

--- python_scripts/clock.py
+++ python_scripts/clock.py
@@ -3,7 +3,6 @@ import os
 import pandas as pd
 from sqlalchemy import create_engine
 import geocodio
-print(dir(geocodio))
 client = geocodio.GeocodioClient("454565525ee5444fefef2572155e155e5248221")
 
 def get_secret_variables():
@@ -11,10 +10,6 @@ def get_secret_variables():
         import secret_variables
         return secret_variables.DATABASE_URL, secret_variables.GEOCODIO_API_KEY
     return os.getenv("DATABASE_URL"), os.getenv("GEOCODIO_API_KEY")
-
-# client = geocodio.GeocodioClient(get_secret_variables()[1])
-# geocoded_location = client.geocode("42370 Bob Hope Drive, Rancho Mirage CA")
-# print(geocoded_location.coords)
 
 def scheduled_job():
     print('and again...')
