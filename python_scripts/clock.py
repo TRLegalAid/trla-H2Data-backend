809b83a8772e8d10fe137e8a0b20244b7d2d3c46
alpiazza13 <alexpiazza2000@gmail.com.com>
2020-08-26 13:53:31 -0400

    removes scheduling

--- python_scripts/clock.py
+++ python_scripts/clock.py
@@ -8,14 +8,14 @@ from populate_database import populate_database
 
 print("made it after the import statemnts of clock.py...")
 
-def scheduled_job():
-    print("made it into the scheduled job script...")
-    populate_database()
+# def scheduled_job():
+#     print("made it into the scheduled job script...")
+populate_database()
 
-sched = BlockingScheduler()
+# sched = BlockingScheduler()
 # change minutes=2 to days=1
 # sched.add_job(scheduled_job, 'interval', days=1, start_date='2020-08-25 22:10:00', timezone='US/Eastern')
-sched.add_job(scheduled_job, 'interval', days=1)
+# sched.add_job(scheduled_job, 'interval', days=1)
 
 
-sched.start()
+# sched.start()
