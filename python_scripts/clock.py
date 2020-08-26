faaa8d639019b5721d777acf33f5ca222e25ca7f
alpiazza13 <alexpiazza2000@gmail.com.com>
2020-08-26 02:17:23 -0400

    testing only clock.py file

--- python_scripts/clock.py
+++ python_scripts/clock.py
@@ -1,7 +1,13 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 import os
-from helpers import get_secret_variables
 import geocodio
+
+def get_secret_variables():
+    if os.getenv("LOCAL_DEV") == "true":
+        import secret_variables
+        return secret_variables.DATABASE_URL, secret_variables.GEOCODIO_API_KEY
+    return os.getenv("DATABASE_URL"), os.getenv("GEOCODIO_API_KEY")
+
 client = geocodio.GeocodioClient(get_secret_variables()[1])
 
 def scheduled_job():
@@ -10,6 +16,6 @@ def scheduled_job():
 
 sched = BlockingScheduler()
 # change minutes=2 to days=1
-sched.add_job(scheduled_job, 'interval', seconds=5, start_date='2020-07-19 22:10:00', timezone='US/Eastern')
+sched.add_job(scheduled_job, 'interval', seconds=3, start_date='2020-07-19 22:10:00', timezone='US/Eastern')
 
 sched.start()
