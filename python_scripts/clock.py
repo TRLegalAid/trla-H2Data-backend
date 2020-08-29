2d70c94c0babae2ba71c8dbaaabdd515c4bdfd52
alpiazza13 <alexpiazza2000@gmail.com.com>
2020-08-29 01:14:14 -0400

    adds idea for checking for changes in low_accuracies

--- python_scripts/clock.py
+++ python_scripts/clock.py
@@ -3,26 +3,24 @@ import os
 import pandas as pd
 from sqlalchemy import create_engine
 import geocodio
-import logging
-logger = logging.Logger('catch_all')
 
-print("made it after the import statemnts of clock.py...")
 
 # def scheduled_job():
 #     print("made it into the scheduled job script...")
 
-try:
-    if x == 4:
-        print("hello")
-except Exception as e:
-    print("There's been a failure, here is the error message:")
-    logger.error(e, exc_info=True)
 
-
-# sched = BlockingScheduler()
+sched = BlockingScheduler()
 # change minutes=2 to days=1
-# sched.add_job(scheduled_job, 'interval', days=1, start_date='2020-08-25 22:10:00', timezone='US/Eastern')
-# sched.add_job(scheduled_job, 'interval', days=1)
+sched.add_job(update, 'interval', days=1, start_date='2020-08-25 22:10:00', timezone='US/Eastern')
+sched.add_job(fix, 'interval', days=1)
 
+sched.start()
 
-# sched.start()
+# for checking for changes in low_accuracies
+# def fix():
+#     old_df = pd.from_sql(low_accuracies)
+#     while True:
+#         new_df = pd.from_sql(low_accuracies)
+#         if not (new_df == old_df).all().all():
+#             run_implement_fixes()
+#         old_df = new_df
