fbf929c3e6b60b826fbffb1deb68644177315f2e
alpiazza13 <alexpiazza2000@gmail.com.com>
2020-08-29 01:36:58 -0400

    testing scheduler

--- python_scripts/clock.py
+++ python_scripts/clock.py
@@ -10,10 +10,11 @@ import geocodio
 def job():
     for i in range(5):
         print(i)
+    print("all done")
 
 sched = BlockingScheduler()
 # change minutes=2 to days=1
-sched.add_job(schedule, 'interval', seconds=5, start_date='2020-08-25 22:10:00', timezone='US/Eastern')
+sched.add_job(job, 'interval', seconds=5, start_date='2020-08-25 22:10:00', timezone='US/Eastern')
 # sched.add_job(fix, 'interval', days=1)
 
 sched.start()
