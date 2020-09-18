08d385e3af27b56b5ccb7ff4843554a5f98817f0
alpiazza13 <alexpiazza2000@gmail.com.com>
2020-09-17 20:27:38 -0400

    debugging overwrite task

--- scripts/overwrite_arcgis.py
+++ scripts/overwrite_arcgis.py
@@ -16,7 +16,13 @@ def overwrite_feature(username, password, new_df, old_feature_name):
     csv_file_name = f"{old_feature_name}.csv"
     new_df.to_csv(csv_file_name)
 
-    old_jobs_item = gis.content.search(f"title: {old_feature_name}", 'Feature Layer')[0]
+    myprint("before searching for item")
+    # old_jobs_item = gis.content.search(f"title: {old_feature_name}", 'Feature Layer')[0]
+    old_jobs = gis.content.search(f"title: {old_feature_name}", 'Feature Layer')
+    myprint("old jobs is:")
+    print(old_jobs)
+    old_jobs_item = old_jobs[0]
+    myprint("after searching for item")
     old_feature_layer = FeatureLayerCollection.fromitem(old_jobs_item)
 
     myprint(f"Overwriting feature layer.... there will now be {len(new_df)} features.")
