4ff0628431537447badf6b5faeec4cd0fd0eabf9
alpiazza13 <alexpiazza2000@gmail.com.com>
2020-10-08 21:45:20 -0400

    remove check in from schedule

--- scripts/schedule.py
+++ scripts/schedule.py
@@ -37,6 +37,6 @@ sched = BlockingScheduler()
 sched.add_job(update_task, 'interval', days=1, start_date='2020-09-09 17:15:00', timezone='US/Eastern')
 sched.add_job(implement_fixes_task, 'interval', hours=6, start_date='2020-09-10 18:00:00', timezone='US/Eastern')
 # sched.add_job(check_for_duplicates_task, 'interval', days=1, start_date='2020-09-08 22:45:00', timezone='US/Eastern')
-sched.add_job(check_in, 'interval', hours=3, start_date='2020-09-08 00:30:00', timezone='US/Eastern')
+# sched.add_job(check_in, 'interval', hours=3, start_date='2020-09-08 00:30:00', timezone='US/Eastern')
 
 sched.start()
