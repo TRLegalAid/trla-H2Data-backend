6467ae8806ff1d60496628ac6176e0add2238071
alpiazza13 <alexpiazza2000@gmail.com.com>
2020-11-12 17:46:35 -0500

    done testing that

--- scripts/schedule.py
+++ scripts/schedule.py
@@ -30,12 +30,9 @@ def mark_inactive_low_accurates_as_fixed_task():
     perform_task_and_catch_errors(mark_all_inactive_low_accurates_as_fixed, "MARKING INACTIVE LOW ACCURACIES AS FIXED")
     perform_task_and_catch_errors(send_fixes_to_postgres, "IMPLEMENTING FIXES")
 
-
-mark_inactive_low_accurates_as_fixed_task()
-
 # update database at 1:00 am EST every day, check for fixes every 6 hours
 sched = BlockingScheduler()
 sched.add_job(update_task, 'interval', days=1, start_date='2020-09-09 01:00:00', timezone='US/Eastern')
 sched.add_job(implement_fixes_task, 'interval', hours=6, start_date='2020-09-10 18:00:00', timezone='US/Eastern')
-sched.add_job(mark_inactive_low_accurates_as_fixed_task, 'interval', days=1, start_date='2020-09-10 20:00:00', timezone='US/Eastern')
+sched.add_job(mark_inactive_low_accurates_as_fixed_task, 'interval', weeks=1, start_date='2020-09-10 20:00:00', timezone='US/Eastern')
 sched.start()
