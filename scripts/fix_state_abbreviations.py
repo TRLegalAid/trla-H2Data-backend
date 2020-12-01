f872efc990ae62fdd035d41e0935477a44efa982
alpiazza13 <alexpiazza2000@gmail.com.com>
2020-12-01 01:47:12 -0500

    uses dict instead of excel for state abbreviations

--- scripts/fix_state_abbreviations.py
+++ scripts/fix_state_abbreviations.py
@@ -2,12 +2,25 @@ import os
 import pandas as pd
 from helpers import make_query
 
+
+states_abbreviations = {'AL': 'Alabama', 'AK': 'Alaska', 'AZ': 'Arizona', 'AR': 'Arkansas', 'CA': 'California',
+                        'CO': 'Colorado', 'CT': 'Connecticut', 'DE': 'Delaware', 'DC': 'District of Columbia',
+                        'FL': 'Florida', 'GA': 'Georgia', 'HI': 'Hawaii', 'ID': 'Idaho', 'IL': 'Illinois', 'IN': 'Indiana',
+                        'IA': 'Iowa', 'KS': 'Kansas', 'KY': 'Kentucky', 'LA': 'Louisiana', 'ME': 'Maine', 'MD': 'Maryland',
+                        'MA': 'Massachusetts', 'MI': 'Michigan', 'MN': 'Minnesota', 'MS': 'Mississippi', 'MO': 'Missouri',
+                        'MT': 'Montana', 'NE': 'Nebraska', 'NV': 'Nevada', 'NH': 'New Hampshire', 'NJ': 'New Jersey',
+                        'NM': 'New Mexico', 'NY': 'New York', 'NC': 'North Carolina', 'ND': 'North Dakota',
+                        'OH': 'Ohio', 'OK': 'Oklahoma', 'OR': 'Oregon', 'PA': 'Pennsylvania', 'RI': 'Rhode Island',
+                        'SC': 'South Carolina', 'SD': 'South Dakota', 'TN': 'Tennessee', 'TX': 'Texas', 'UT': 'Utah',
+                        'VT': 'Vermont', 'VA': 'Virginia', 'WA': 'Washington', 'WV': 'West Virginia', 'WI': 'Wisconsin',
+                        'WY': 'Wyoming', 'PR': 'Puerto Rico', 'MP': 'Northern Mariana Islands', 'VI': 'U.S. Virgin Islands'}
+
+
 # converts all abbreviations in the WORKSITE_STATE column of job_central to full state names
 def expand_abbreviations():
-    states_with_abbreviations = pd.read_excel("../excel_files/state_abbreviations.xlsx")
 
-    for i, row in states_with_abbreviations.iterrows():
-        state, abbreviation = row["State"], row["Abbreviation"]
+    for abbreviation in states_abbreviations:
+        state = states_abbreviations[abbreviation]
         make_query(f"""UPDATE job_central
                        SET "WORKSITE_STATE" = '{state}'
                        WHERE "WORKSITE_STATE" IN ('{abbreviation}', '{abbreviation.lower()}')""")
