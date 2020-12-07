e0960a54cb93f3746e9b074f502e3785f038a8ce
alpiazza13 <alexpiazza2000@gmail.com.com>
2020-12-07 11:38:52 -0500

    fixes bug

--- scripts/low_accuracies_google_sheet.py
+++ scripts/low_accuracies_google_sheet.py
@@ -106,5 +106,6 @@ def send_fixes_in_our_google_sheet_to_low_accuracies():
     send_sheet_fixes_to_postgres(our_sheet)
 
 if __name__ == "__main__":
+    pass
     # replace_our_google_sheet_with_low_accuracies_table()
     # send_fixes_in_our_google_sheet_to_low_accuracies()
