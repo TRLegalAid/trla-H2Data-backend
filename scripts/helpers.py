77fb49e80d3f7b001a96f98b2d9c138b5511174f
alpiazza13 <alexpiazza2000@gmail.com.com>
2021-04-23 18:47:51 -0400

    small change

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -285,7 +285,7 @@ def fix_zip_code(zip_code):
     if isinstance(zip_code, str):
         return ("0" * (5 - len(zip_code))) + zip_code
     elif zip_code == None or isnan(zip_code):
-        return None
+        return ""
     else:
         zip_code = str(int(zip_code))
         return ("0" * (5 - len(zip_code))) + zip_code
